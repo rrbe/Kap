98f5ab38657d27167bb6de72693fed8c2eab4aa8
shawn <shineoutgx@gmail.com>
2026-07-21 00:47:49 +0800

    chore: upgrade sentry sdk

--- renderer/utils/sentry-error-boundary.tsx
+++ renderer/utils/sentry-error-boundary.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import * as Sentry from '@sentry/browser';
+import * as Sentry from '@sentry/electron/renderer';
 import {settings} from './settings';
-
-const SENTRY_PUBLIC_DSN = 'https://2dffdbd619f34418817f4db3309299ce@sentry.io/255536';
 
 class SentryErrorBoundary extends React.Component<{children: React.ReactNode}> {
   constructor(props) {
@@ -10,18 +8,16 @@ class SentryErrorBoundary extends React.Component<{children: React.ReactNode}> {
     const app = window.kap.app.getInfo();
 
     if (!app.development && settings.get('allowAnalytics')) {
-      const release = `${app.name}@${app.version}`.toLowerCase();
-      Sentry.init({dsn: SENTRY_PUBLIC_DSN, release});
+      Sentry.init();
     }
   }
 
   componentDidCatch(error, errorInfo) {
     console.log(error, errorInfo);
-    Sentry.configureScope(scope => {
-      for (const [key, value] of Object.entries(errorInfo)) {
-        scope.setExtra(key, value);
-      }
-    });
+    const scope = Sentry.getCurrentScope();
+    for (const [key, value] of Object.entries(errorInfo)) {
+      scope.setExtra(key, value);
+    }
 
     Sentry.captureException(error);
 
